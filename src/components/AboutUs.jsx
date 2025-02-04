7862191464d3d70c7941c2a2b3c77f6d2cd3c1d0
MakulaMilanKevin <norBMW@outlook.hu>
2025-02-04 09:08:00 +0100

    Still Base
    IMGS

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
    <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      src="https://avatars.githubusercontent.com/u/134051404?v=4"
       className="max-w-sm rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
@@ -21,7 +21,7 @@ const AboutUs = () => {
 <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      src="https://avatars.githubusercontent.com/u/92539699?v=4"
       className="max-w-sm rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
