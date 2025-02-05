1685cc59bd6f7cfe7f439adb2a9e668b6dd742be
MakulaMilanKevin <norBMW@outlook.hu>
2025-02-05 10:01:20 +0100

    LOL

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div>
+      <div className="avatar">
+  <div className="w-24 rounded-full">
+    <img src='#kepek' />
+  </div>
+</div>
+      <input type="file" id='kepek'  className="file-input file-input-bordered w-full max-w-xs" />
+      </div>
   )
 }
 
