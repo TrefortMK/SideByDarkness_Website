0ba624a2a6af00f8d2e89e9ea269070871a95f6b
MakulaMilanKevin <norBMW@outlook.hu>
2025-03-18 08:51:29 +0100

    Update Settings.jsx

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,72 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
 
 const Settings = () => {
+  
+    const [theme, setTheme] = useState(Cookies.get('theme') || 'light');
+
+  const toggleTheme = (themeType) => {
+    Cookies.set('theme', themeType);
+    setTheme(themeType);
+  };
+
   return (
-    <>
-    <div className="justify-center items-center gap-5">
-  <label className="flex cursor-pointer gap-2">
-  <span className="label-text">Current</span>
-  <input type="checkbox" value="synthwave" className="toggle theme-controller" />
-  <span className="label-text">Synthwave</span>
-</label>
-<div className="dropdown mb-72">
-  <div tabIndex={0} role="button" className="btn m-1">
-    Theme
-    <svg
-      width="12px"
-      height="12px"
-      className="inline-block h-2 w-2 fill-current opacity-60"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 2048 2048">
-      <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-    </svg>
-  </div>
-  <ul tabIndex={0} className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl">
-    <li>
-      <input
-        type="radio"
-        name="theme-dropdown"
-        className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-        aria-label="Default"
-        value="default" />
-    </li>
-    <li>
-      <input
-        type="radio"
-        name="theme-dropdown"
-        className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-        aria-label="Retro"
-        value="retro" />
-    </li>
-    <li>
-      <input
-        type="radio"
-        name="theme-dropdown"
-        className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-        aria-label="Cyberpunk"
-        value="cyberpunk" />
-    </li>
-    <li>
-      <input
-        type="radio"
-        name="theme-dropdown"
-        className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-        aria-label="Valentine"
-        value="valentine" />
-    </li>
-    <li>
-      <input
-        type="radio"
-        name="theme-dropdown"
-        className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-        aria-label="Aqua"
-        value="aqua" />
-    </li>
-  </ul>
-</div>
-</div>
-    </>
-  )
+    <div>
+      <button onClick={() => toggleTheme('light')}>Light Theme</button>
+      <button onClick={() => toggleTheme('dark')}>Dark Theme</button>
+      <p>Current theme is {theme}</p>
+    </div>
+  );
+
 }
 
 export default Settings
